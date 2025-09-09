b27cf701586efcb412f352dac7e0e9c8916f7162
vishalthakur06 <vishalultralegend612@gmail.com>
2025-09-09 19:36:16 +0530

    Remove unnecesary code

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-    server: {
-    host: true, // expose to network
-    port: 5173, // optional: set port
-  },
-})
+  plugins: [react(), tailwindcss()],
+});
